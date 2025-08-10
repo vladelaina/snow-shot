45aae89d48d87899ea90f01c0db1dba7935f9dca
chao <chao@mgchao.top>
2025-08-10 23:18:08 +0800

    fix: 修复截图窗口多显示器的弹窗定位问题

--- src/app/contextWrap.tsx
+++ src/app/contextWrap.tsx
@@ -342,7 +342,7 @@ export const defaultAppSettingsData: AppSettingsData = {
     },
     [AppSettingsGroup.SystemScreenshot]: {
         tryGetElementByFocus: TryGetElementByFocus.WhiteList,
-        ocrModel: OcrModel.RapidOcrV5,
+        ocrModel: OcrModel.RapidOcrV4,
         ocrDetectAngle: false,
     },
 };
